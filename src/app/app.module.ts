64245f63b2bd60492d1b0dfcb679fa56697b9f7c
Sai Kishore <saikishorechoragudi@gmail.com>
2023-06-25 20:07:31 +0530

    Added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SharedModule } from "./shared/shared.module";
+import { UserModule } from "./user/user.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavComponent,
+        
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        UserModule
+    ]
 })
 export class AppModule { }
---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  //providers: [ModalService]
+})
+export class ModalComponent  {
+  constructor(public modal: ModalService) {}
+    closeModal(){
+      
+  
+      this.modal.toggleModal()
+    
+  }
+
+
+
+
+  
+}
